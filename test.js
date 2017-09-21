425508cb28cd88e0703dd6ee33879bdc1a0af6ca
Roman <bondrogeen@gmail.com>
2017-09-21 23:25:50 +0300

    1

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-var hilink = require('hilinkhuawei');
-
-
-hilink.status(function (response) {
-    console.log(JSON.stringify(response, null, 2));
-});
+//var hilink = require('hilinkhuawei');
+//
+//
+//hilink.status(function (response) {
+//    console.log(JSON.stringify(response, null, 2));
+//});
